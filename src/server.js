ae7e09075c768b3cc148f92e12242758460a44fa
Daniel Libanori <daniel.lb68@gmail.com>
2023-11-17 17:44:32 -0300

    PORT update

--- src/server.js
+++ src/server.js
@@ -11,4 +11,4 @@ app.use(router);
 
 
 
-app.listen(process.env.PORT, () => console.log("Server listening on port 3030"));
+app.listen(process.env.PORT || 3000, () => console.log("Server listening on port 3030"));
